a8ce8cdff7b82de0179b095f495a1014a20eb7de
Jared Jeseo <jjeseo@icloud.comnane README.txt cleargit config --global user.email jjeseo@icloud.com>
2018-07-19 19:53:44 -0400

    Updated js formfun.js

--- js/formfun.js
+++ js/formfun.js
@@ -4,8 +4,8 @@ function getvalue() {
     var firstName = document.getElementById("firstNameInput").value;
     var userEmail = document.getElementById("InputEmail1").value;
 
-    console.log(firstName);
-    console.log(userEmail);
+    // console.log(firstName);
+    // console.log(userEmail);
 
     document.getElementById("ModalLongTitle").innerHTML = "Thank you " + firstName + "!";
     document.getElementById("messageForm").innerHTML = "You'll be receiving an email at " + userEmail + " soon! Be on the look out for emails! Lots of good things to come.";
