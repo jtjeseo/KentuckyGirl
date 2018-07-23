e7fcca77bd1ab4d0df16b6565897a66778c33203
Jared Jeseo <jjeseo@icloud.comnane README.txt cleargit config --global user.email jjeseo@icloud.com>
2018-07-23 19:02:12 -0400

    Updated javascript for archive page. Changed it to JQuery

--- js/accordian.js
+++ js/accordian.js
@@ -1,18 +1,25 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+// let accordion = document.getElementsByClassName("accordion");
+// let i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
+// for (i = 0; i < accordion.length; i++) {
+//     accordion[i].addEventListener("click", function () {
+//         /* Toggle between adding and removing the "active" class,
+//         to highlight the button that controls the panel */
+//         this.classList.toggle("active");
 
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+//         /* Toggle between hiding and showing the active panel */
+//         $(this).next().slideToggle();
+//         // if (panel.style.display === "block") {
+//         //     panel.style.display = "none";
+//         // } else {
+//         //     panel.style.display = "block";
+//         // }
+//     });
+// }
+
+
+$('.accordion').each(function () {
+    $(this).click(function () {
+        $(this).next().slideToggle();
     });
-}
+});
