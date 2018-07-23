ba8bcc8180fb74cb29d65e8c024118ef9ae1b6b3
Jared Jeseo <jjeseo@icloud.comnane README.txt cleargit config --global user.email jjeseo@icloud.com>
2018-07-23 19:52:19 -0400

    updated accordian.js with JQuery

--- js/accordian.js
+++ js/accordian.js
@@ -1,23 +1,3 @@
-// let accordion = document.getElementsByClassName("accordion");
-// let i;
-
-// for (i = 0; i < accordion.length; i++) {
-//     accordion[i].addEventListener("click", function () {
-//         /* Toggle between adding and removing the "active" class,
-//         to highlight the button that controls the panel */
-//         this.classList.toggle("active");
-
-//         /* Toggle between hiding and showing the active panel */
-//         $(this).next().slideToggle();
-//         // if (panel.style.display === "block") {
-//         //     panel.style.display = "none";
-//         // } else {
-//         //     panel.style.display = "block";
-//         // }
-//     });
-// }
-
-
 $('.accordion').each(function () {
     $(this).click(function () {
         $(this).next().slideToggle();
